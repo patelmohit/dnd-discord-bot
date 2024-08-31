42fee849899f08a8bbb30079fa06736650f0520b
Mohit Patel <m265pate@uwaterloo.ca>
2024-08-30 20:17:38 -0400

    add echo command

--- commands.js
+++ commands.js
@@ -26,24 +26,18 @@ const TEST_COMMAND = {
   contexts: [0, 1, 2],
 };
 
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
+
+
+// Echo back the second argument baby
+const ECHO_COMMAND = {
+  name: 'echo',
+  description: 'Return back what you send!',
   type: 1,
   integration_types: [0, 1],
-  contexts: [0, 2],
+  contexts: [0, 1, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+
+const ALL_COMMANDS = [TEST_COMMAND, ECHO_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
