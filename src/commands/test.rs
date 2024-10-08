a6f38a7678a2e842c8905307a2bfb132562b1b54
Mohit Patel <m265pate@uwaterloo.ca>
2024-10-07 20:22:38 -0400

    lets do a test command - in rust!!

---
+++ src/commands/test.rs
@@ -0,0 +1,12 @@
+use serenity::builder::CreateCommand;
+use serenity::model::application::ResolvedOption;
+
+// https://github.com/serenity-rs/serenity/blob/current/examples/e14_slash_commands/src/commands/ping.rs
+
+pub fn run(_options: &[ResolvedOption]) -> String {
+    "meow meow".to_string()
+}
+
+pub fn register() -> CreateCommand {
+    CreateCommand::new("test").description("I'll meow if I'm working!")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod commands;
+
 use std::env;
 
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+
+use serenity::model::id::GuildId;
 use serenity::prelude::*;
 
 struct Handler;
@@ -14,15 +18,31 @@ impl EventHandler for Handler {
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 println!("Error sending message: {why:?}");
             }
-        } else if msg.content == "!test" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "rust meow?").await {
-                println!("Error sending message: {why:?}");
-            }
         }
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId::new(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let commands = guild_id
+            .set_commands(&ctx.http, vec![
+                commands::test::register()
+            ])
+            .await;
+
+        println!("I now have the following guild slash commands: {commands:#?}");
+
+        // let guild_command =
+        //     Command::create_global_command(&ctx.http, commands::wonderful_command::register())
+        //         .await;
+
     }
 }
 
@@ -35,8 +55,10 @@ async fn main() {
     let intents = GatewayIntents::GUILD_MESSAGES;
 
     // Create a new instance of the Client, logging in as a bot.
-    let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
 
     // Start listening for events by starting a single shard
     if let Err(why) = client.start().await {
