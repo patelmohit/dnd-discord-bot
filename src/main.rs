dcd04244232689395ec267e0ee0ec2ea6f0a3658
Mohit Patel <m265pate@uwaterloo.ca>
2024-10-07 21:37:32 -0400

    try guild command

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use std::env;
 
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{self as serenity, GuildId};
 
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
-async fn test(
-    ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
-) -> Result<(), Error> {
+#[poise::command(slash_command)]
+async fn test(ctx: Context<'_>) -> Result<(), Error> {
     let response = "meow meow from Rust!";
     ctx.say(response).await?;
     Ok(())
@@ -22,15 +19,22 @@ async fn main() {
     dotenv::dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN in the environment");
     let intents = serenity::GatewayIntents::GUILD_MESSAGES;
+    let guild_id = GuildId::new(
+        env::var("GUILD_ID")
+            .expect("Expected GUILD_ID in environment")
+            .parse()
+            .expect("GUILD_ID must be an integer"),
+    );
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![test()],
             ..Default::default()
         })
-        .setup(|ctx, _ready, framework| {
+        .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                poise::builtins::register_in_guild(ctx, &framework.options().commands, guild_id)
+                    .await?;
                 Ok(Data {})
             })
         })
