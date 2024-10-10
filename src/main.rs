5d720278059a68b65cef5c82a4da47b43ce22cc6
Mohit Patel <m265pate@uwaterloo.ca>
2024-10-09 20:57:22 -0400

    ping pong wee

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,18 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+    // Set a handler for the `message` event. This is called whenever a new message is received.
+    //
+    // Event handlers are dispatched through a threadpool, and so multiple events can be
+    // dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
-        if let Err(why) = msg.channel_id.say(&ctx.http, "hi there!").await {
-            println!("Error sending message: {:?}", why);
+        if msg.content == "!ping" {
+            // Sending a message can fail, due to a network error, an authentication error, or lack
+            // of permissions to post in the channel, so log to stdout when some error happens,
+            // with a description of it.
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
         }
     }
 
